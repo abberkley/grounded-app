926d6cbcb6a1387fc505786ddfc768df6611a56c
abberkley <69809468+abberkley@users.noreply.github.com>
2022-12-12 03:55:16 -0500

    got rid of duplicate data

--- src/constants/data.js
+++ src/constants/data.js
@@ -5,24 +5,6 @@ const menu = [
 		description:
 			'One quick way to perk up instantly. Our take on a regular espresso.',
 	},
-	{
-		title: 'wake up call',
-		price: '$3.49',
-		description:
-			'One quick way to perk up instantly. Our take on a regular espresso.',
-	},
-	{
-		title: 'wake up call',
-		price: '$3.49',
-		description:
-			'One quick way to perk up instantly. Our take on a regular espresso.',
-	},
-	{
-		title: 'wake up call',
-		price: '$3.49',
-		description:
-			'One quick way to perk up instantly. Our take on a regular espresso.',
-	},
 ];
 
 export default { menu };
